8c46d14ba1abfc386fecf5b8cc41fefb003f5241
Olvander <olli.pertovaara@gmail.com>
2024-01-11 14:36:45 +0200

    Update file BicycleJourney.java
    Add three constructors, one default and two other with parameters

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/journeys/BicycleJourney.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/journeys/BicycleJourney.java
@@ -11,4 +11,31 @@ public class BicycleJourney {
     private String returnStationId;
     private double coveredDistance;
     private int journeyDuration;
+
+    public BicycleJourney() {}
+
+    public BicycleJourney(LocalDateTime departureDate, LocalDateTime returnDate,
+                          String departureStationId, String returnStationId,
+                          double coveredDistance, int journeyDuration) {
+
+        this.departureDate = departureDate;
+        this.returnDate = returnDate;
+        this.departureStationId = departureStationId;
+        this.returnStationId = returnStationId;
+        this.coveredDistance = coveredDistance;
+        this.journeyDuration = journeyDuration;
+    }
+
+    public BicycleJourney(Long id, LocalDateTime departureDate, LocalDateTime
+            returnDate, String departureStationId, String returnStationId,
+                          double coveredDistance, int journeyDuration) {
+
+        this.id = id;
+        this.departureDate = departureDate;
+        this.returnDate = returnDate;
+        this.departureStationId = departureStationId;
+        this.returnStationId = returnStationId;
+        this.coveredDistance = coveredDistance;
+        this.journeyDuration = journeyDuration;
+    }
 }
