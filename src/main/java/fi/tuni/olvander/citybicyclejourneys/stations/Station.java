a9fbc27bb2eaadaa5696b95bbe5223f8c245baac
Olvander <olli.pertovaara@gmail.com>
2024-01-11 14:09:37 +0200

    Add six fields to Station.java
    For setting and getting Station data

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/Station.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/Station.java
@@ -1,4 +1,10 @@
 package fi.tuni.olvander.citybicyclejourneys.stations;
 
 public class Station {
+    private int id;
+    private String stationId;
+    private String name;
+    private String address;
+    private double x;
+    private double y;
 }
