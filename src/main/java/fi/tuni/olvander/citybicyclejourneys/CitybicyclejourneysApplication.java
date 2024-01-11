2c8f13e792202afc5e60a1b5bf09cd1fadb4a0da
Olvander <olli.pertovaara@gmail.com>
2024-01-11 11:19:53 +0200

    Add multiple files
    To initialize project

---
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/CitybicyclejourneysApplication.java
@@ -0,0 +1,13 @@
+package fi.tuni.olvander.citybicyclejourneys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitybicyclejourneysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitybicyclejourneysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/tuni/olvander/citybicyclejourneys/CitybicyclejourneysApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.tuni.olvander.citybicyclejourneys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitybicyclejourneysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
