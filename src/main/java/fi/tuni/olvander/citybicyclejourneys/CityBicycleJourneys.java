642dc10bce1c9ccac34ac255e6db9b4711e04187
Olvander <olli.pertovaara@gmail.com>
2024-01-11 12:24:38 +0200

    Rename file CitybicyclejourneysApplication.java
    To make its name more concise

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/CityBicycleJourneys.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/CityBicycleJourneys.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CitybicyclejourneysApplication {
+public class CityBicycleJourneys {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CitybicyclejourneysApplication.class, args);
+		SpringApplication.run(CityBicycleJourneys.class, args);
 	}
 
 }
