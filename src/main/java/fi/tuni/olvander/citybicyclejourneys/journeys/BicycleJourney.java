b4c2dbaa31fea30caa839b4b7141711c146c1eb4
Olvander <olli.pertovaara@gmail.com>
2024-01-11 14:32:08 +0200

    Add seven fields to class BicycleJourney.java
    For creating and getting BicycleJourney objects

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/journeys/BicycleJourney.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/journeys/BicycleJourney.java
@@ -1,4 +1,14 @@
 package fi.tuni.olvander.citybicyclejourneys.journeys;
 
+import java.time.LocalDateTime;
+
 public class BicycleJourney {
+
+    private Long id;
+    private LocalDateTime departureDate;
+    private LocalDateTime returnDate;
+    private String departureStationId;
+    private String returnStationId;
+    private double coveredDistance;
+    private int journeyDuration;
 }
