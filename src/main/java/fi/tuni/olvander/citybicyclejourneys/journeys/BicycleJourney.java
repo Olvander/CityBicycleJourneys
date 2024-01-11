159dea182b0bb184c6a1b3214dd6916eb283adb9
Olvander <olli.pertovaara@gmail.com>
2024-01-11 14:41:57 +0200

    Update file BicycleJourney
    Add @Entity and Id generation related annotations to the class

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/journeys/BicycleJourney.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/journeys/BicycleJourney.java
@@ -1,9 +1,17 @@
 package fi.tuni.olvander.citybicyclejourneys.journeys;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDateTime;
 
+@Entity
 public class BicycleJourney {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime departureDate;
     private LocalDateTime returnDate;
