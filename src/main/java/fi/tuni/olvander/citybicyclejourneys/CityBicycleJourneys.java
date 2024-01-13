cbfe7548202eb48b82dad99a9e14b2599bb73eec
Olvander <olli.pertovaara@gmail.com>
2024-01-13 13:01:15 +0200

    Update CityBicycleJourneys
    Add a method importStationsFrom() to import Stations from a CSV file

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/CityBicycleJourneys.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/CityBicycleJourneys.java
@@ -1,12 +1,18 @@
 package fi.tuni.olvander.citybicyclejourneys;
 
+import fi.tuni.olvander.citybicyclejourneys.journeys.BicycleJourney;
 import fi.tuni.olvander.citybicyclejourneys.journeys.BicycleJourneyRepository;
+import fi.tuni.olvander.citybicyclejourneys.stations.Station;
 import fi.tuni.olvander.citybicyclejourneys.stations.StationRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.h2.tools.Csv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.ResultSet;
+import java.util.Optional;
 
 @SpringBootApplication
 public class CityBicycleJourneys {
@@ -24,5 +30,20 @@ public class CityBicycleJourneys {
 	}
 
 
+	public synchronized void importStationsFrom(String file) {
+
+		try {
+			ResultSet rs = new Csv().read(file, null, null);
+
+			if (rs != null) {
+
+				while (rs.next()) {
+
+				}
+			}
+		} catch (Exception e) {
+			logger.info("Could not read file, please check the file name");
+		}
+	}
 
 }
