1f11bd7d6182d7ac911eb60a8acac30c747e7775
Olvander <olli.pertovaara@gmail.com>
2024-01-13 12:46:18 +0200

    Update file CityBicycleJourneys.java
    Create an autowired instance of StationRepository

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/CityBicycleJourneys.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/CityBicycleJourneys.java
@@ -1,10 +1,16 @@
 package fi.tuni.olvander.citybicyclejourneys;
 
+import fi.tuni.olvander.citybicyclejourneys.journeys.BicycleJourneyRepository;
+import fi.tuni.olvander.citybicyclejourneys.stations.StationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CityBicycleJourneys {
+
+    @Autowired
+	StationRepository stationDb;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CityBicycleJourneys.class, args);
