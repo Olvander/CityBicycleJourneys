aa95f5600ae481ca3a29d4f4e2a120c5c6b3d32b
Olvander <olli.pertovaara@gmail.com>
2024-01-14 14:56:57 +0200

    Update file StationController.java
    Add @Controller annotation to the class

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/StationController.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/StationController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +17,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Optional;
 
+@Controller
 public class StationController {
 
     @Autowired
