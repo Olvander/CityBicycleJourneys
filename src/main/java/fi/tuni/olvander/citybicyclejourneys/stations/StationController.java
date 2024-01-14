5c6e31ea2a560eea6410caa96c0f7cb719c752dc
Olvander <olli.pertovaara@gmail.com>
2024-01-14 12:23:08 +0200

    Update StationController.java
    Add an autowired instance of the Station Repository

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/StationController.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/StationController.java
@@ -1,4 +1,9 @@
 package fi.tuni.olvander.citybicyclejourneys.stations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class StationController {
+
+    @Autowired
+    private StationRepository stationDb;
 }
