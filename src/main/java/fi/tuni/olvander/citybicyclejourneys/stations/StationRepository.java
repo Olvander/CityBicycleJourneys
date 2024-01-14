4a54d251d392e8551da6fef39ca06bcc122b0807
Olvander <olli.pertovaara@gmail.com>
2024-01-14 14:44:40 +0200

    Update file StationRepository.java
    Add method findByStationId() for finding a Station

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/StationRepository.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/StationRepository.java
@@ -2,5 +2,9 @@ package fi.tuni.olvander.citybicyclejourneys.stations;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface StationRepository extends CrudRepository<Station, Integer> {
+
+    Optional<Station> findByStationId(String stationId);
 }
