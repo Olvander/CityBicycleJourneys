b843c75850c46c5d211ff48ad67e062dc4350c12
Olvander <olli.pertovaara@gmail.com>
2024-01-14 12:34:18 +0200

    Update StationController.java
    Add a method getStations() to get all Station Objects from an endpoint

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/StationController.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/StationController.java
@@ -1,7 +1,12 @@
 package fi.tuni.olvander.citybicyclejourneys.stations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 public class StationController {
 
@@ -12,4 +17,19 @@ public class StationController {
     private JdbcTemplate jdbcTemplate;
 
     public StationController() {}
+
+    @RequestMapping(value = "api/stations/", method = RequestMethod.GET)
+    public synchronized ResponseEntity<Iterable<Station>> getStations() {
+
+        if (stationDb.count() > 0) {
+            Iterable<Station> stations = stationDb.findAll();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccessControlAllowOrigin("*");
+
+            return new ResponseEntity<>(stations, headers, HttpStatus.OK);
+        } else {
+
+            return null;
+        }
+    }
 }
