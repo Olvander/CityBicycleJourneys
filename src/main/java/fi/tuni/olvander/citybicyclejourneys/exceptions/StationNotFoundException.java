30a0350588a8080b8f8225a93e6415d864e4f371
Olvander <olli.pertovaara@gmail.com>
2024-01-14 13:03:42 +0200

    Update file StationNotFoundException.java
    Add a parameterized constructor, a get method and an id field

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/StationNotFoundException.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/StationNotFoundException.java
@@ -1,4 +1,13 @@
 package fi.tuni.olvander.citybicyclejourneys.exceptions;
 
 public class StationNotFoundException extends IllegalArgumentException {
+    private final int id;
+
+    public StationNotFoundException(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
