a660638b065b43ae80626b3233288ab72d9ed68c
Olvander <olli.pertovaara@gmail.com>
2024-01-14 12:56:08 +0200

    Update file IdNotANumberException
    Add a parameterized constructor, a get method and an id field

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/IdNotANumberException.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/IdNotANumberException.java
@@ -1,4 +1,14 @@
 package fi.tuni.olvander.citybicyclejourneys.exceptions;
 
 public class IdNotANumberException extends NumberFormatException {
+
+    private final String id;
+
+    public IdNotANumberException(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
