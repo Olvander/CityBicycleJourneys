656246a33e2870317debb436f9b7d6a59174fef0
Olvander <olli.pertovaara@gmail.com>
2024-01-14 12:25:29 +0200

    Update StationController.java
    Add an autowired JdbcTemplate to interact with the database tables

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/StationController.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/stations/StationController.java
@@ -1,9 +1,13 @@
 package fi.tuni.olvander.citybicyclejourneys.stations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class StationController {
 
     @Autowired
     private StationRepository stationDb;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 }
