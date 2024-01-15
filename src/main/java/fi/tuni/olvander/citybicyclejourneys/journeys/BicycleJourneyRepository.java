e9e244ea01aac44e23b198fd34da42359252b3cd
Olvander <olli.pertovaara@gmail.com>
2024-01-15 12:56:25 +0200

    Update file BicycleJourneyRepository.java
    Add Javadoc comments

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/journeys/BicycleJourneyRepository.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/journeys/BicycleJourneyRepository.java
@@ -2,6 +2,14 @@ package fi.tuni.olvander.citybicyclejourneys.journeys;
 
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * A Bicycle Journey Repository interface that extends CrudRepository. The<br/>
+ * types for interacting with the database are BicycleJourney and a Long id.
+ *
+ * @author  Olli Pertovaara
+ * @version 2023.12.13
+ * @since   1.21
+ */
 public interface BicycleJourneyRepository extends CrudRepository
         <BicycleJourney, Long> {
 }
