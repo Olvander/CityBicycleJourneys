eb6417022ea1c0cde9eecb4c69dd183f913319e5
Olvander <olli.pertovaara@gmail.com>
2024-01-15 12:41:29 +0200

    Update file IdNotANumberException.java
    Add Javadoc comments

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/IdNotANumberException.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/IdNotANumberException.java
@@ -1,13 +1,33 @@
 package fi.tuni.olvander.citybicyclejourneys.exceptions;
 
+/**
+ * A class for returning the id that is not in a number format.
+ *
+ * @author  Olli Pertovaara
+ * @version 2023.12.13
+ * @since   1.21
+ */
 public class IdNotANumberException extends NumberFormatException {
 
+    /**
+     * A final id in String format.
+     */
     private final String id;
 
+    /**
+     * A constructor for saving and displaying the id not in a number format.
+     *
+     * @param id The id that is not in a number format
+     */
     public IdNotANumberException(String id) {
         this.id = id;
     }
 
+    /**
+     * A getter method for the exception information.
+     *
+     * @return The String containing the exception information
+     */
     public String getId() {
         return id;
     }
