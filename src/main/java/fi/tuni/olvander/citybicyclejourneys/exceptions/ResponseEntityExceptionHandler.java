301b8cf2140d11961655474036261aa09e134ae2
Olvander <olli.pertovaara@gmail.com>
2024-01-15 12:51:47 +0200

    Update file ResponseEntityExceptionHandler.java
    Add Javadoc comments

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/ResponseEntityExceptionHandler.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/ResponseEntityExceptionHandler.java
@@ -6,9 +6,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * A class for giving advice regarding Response Entity Exceptions within<br/>
+ * Controller classes. All these methods include header information which<br/>
+ * allow access for all URL origins. The methods throw exceptions in<br/>
+ * Controller classes when Entities cannot be found.
+ *
+ * @author  Olli Pertovaara
+ * @version 2023.12.13
+ * @since   1.21
+ */
 @ControllerAdvice
 public class ResponseEntityExceptionHandler {
 
+    /**
+     * An exception handler method for BicycleJourneys whose ids are in a<br/>
+     * proper format but cannot be found in the database.
+     *
+     * @param  exception An exception stating a BicycleJourney was not found.
+     * @return           A Response Entity with exception information and a<br/>
+     *                   NOT_FOUND HttpStatus.
+     */
     @ExceptionHandler(BicycleJourneyNotFoundException.class)
     public ResponseEntity<ExceptionInfo> returnBicycleJourneyException(
             BicycleJourneyNotFoundException exception) {
@@ -22,6 +40,14 @@ public class ResponseEntityExceptionHandler {
                 HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * An exception handler method for Stations whose id are in a proper<br/>
+     * format but cannot be found in the database.
+     *
+     * @param  exception An exception stating that a Station cannot be found.
+     * @return           A Response Entity with exception information and a<br/>
+     *                   NOT_FOUND HttpStatus.
+     */
     @ExceptionHandler(StationNotFoundException.class)
     public ResponseEntity<ExceptionInfo> returnStationException(
             StationNotFoundException exception) {
@@ -35,6 +61,13 @@ public class ResponseEntityExceptionHandler {
                 HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * An exception handler method for ids that are in a wrong format.
+     *
+     * @param  exception An exception stating that an id is not a number
+     * @return           A Response Entity with exception information and a<br/>
+     *                   NOT_FOUND HttpStatus.
+     */
     @ExceptionHandler(IdNotANumberException.class)
     public ResponseEntity<ExceptionInfo> returnIdException(
             IdNotANumberException exception) {
