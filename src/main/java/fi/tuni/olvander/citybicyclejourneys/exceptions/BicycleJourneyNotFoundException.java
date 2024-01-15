a19e89bf445d34c45364da7477abb7bd3da96887
Olvander <olli.pertovaara@gmail.com>
2024-01-15 11:24:53 +0200

    Update file BicycleJourneyNotFoundException.java
    Add a default constructor, an id field and its get method

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/BicycleJourneyNotFoundException.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/BicycleJourneyNotFoundException.java
@@ -1,4 +1,14 @@
 package fi.tuni.olvander.citybicyclejourneys.exceptions;
 
 public class BicycleJourneyNotFoundException extends IllegalArgumentException {
+
+    private final Long id;
+
+    public BicycleJourneyNotFoundException(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
