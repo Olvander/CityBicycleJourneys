2ca69831670f36bd819b24a0ac2605de252bd343
Olvander <olli.pertovaara@gmail.com>
2024-01-15 12:11:23 +0200

    Update file ExceptionInfo.java
    Add a field, a constructor and methods to set and get exception info

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/ExceptionInfo.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/ExceptionInfo.java
@@ -1,4 +1,18 @@
 package fi.tuni.olvander.citybicyclejourneys.exceptions;
 
 public class ExceptionInfo {
+
+    private String exceptionInfo;
+
+    public ExceptionInfo(String exceptionInfo) {
+        this.exceptionInfo = exceptionInfo;
+    }
+
+    public String getExceptionInfo() {
+        return exceptionInfo;
+    }
+
+    public void setExceptionInfo(String exceptionInfo) {
+        this.exceptionInfo = exceptionInfo;
+    }
 }
