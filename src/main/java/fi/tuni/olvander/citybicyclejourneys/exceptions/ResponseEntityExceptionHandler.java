b5d90fb207afc6433d9627572ffdd7f217be907b
Olvander <olli.pertovaara@gmail.com>
2024-01-15 12:15:41 +0200

    Update file ResponseEntityExceptionHandler.java
    Add method returnBicycleJourneyException()

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/ResponseEntityExceptionHandler.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/ResponseEntityExceptionHandler.java
@@ -1,7 +1,24 @@
 package fi.tuni.olvander.citybicyclejourneys.exceptions;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(BicycleJourneyNotFoundException.class)
+    public ResponseEntity<ExceptionInfo> returnBicycleJourneyException(
+            BicycleJourneyNotFoundException exception) {
+
+        ExceptionInfo exceptionInfo = new ExceptionInfo(
+                "Cannot find BicycleJourney with an id " + exception.getId());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccessControlAllowOrigin("*");
+
+        return new ResponseEntity<>(exceptionInfo, headers,
+                HttpStatus.NOT_FOUND);
+    }
 }
