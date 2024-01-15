4f7f830f7b1f57632cc25ff386d140bc07acd502
Olvander <olli.pertovaara@gmail.com>
2024-01-15 12:55:10 +0200

    Update file StationNotFoundException.java
    Add Javadoc comments

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/StationNotFoundException.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/exceptions/StationNotFoundException.java
@@ -1,12 +1,35 @@
 package fi.tuni.olvander.citybicyclejourneys.exceptions;
 
+/**
+ * This class is for Exceptions where a Station id is in proper format<br/>
+ * but the Station is not found in the Station database.
+ *
+ * @author  Olli Pertovaara
+ * @version 2023.12.13
+ * @since   1.21
+ */
 public class StationNotFoundException extends IllegalArgumentException {
+
+    /**
+     * A final id in integer format.
+     **/
     private final int id;
 
+    /**
+     * A constructor for saving and returning the Station id<br/>
+     * of a Station that is not found in the database.
+     *
+     * @param id An id in integer format to be saved and returned
+     */
     public StationNotFoundException(int id) {
         this.id = id;
     }
 
+    /**
+     * A getter method for the id.
+     *
+     * @return An id in integer format
+     */
     public int getId() {
         return id;
     }
