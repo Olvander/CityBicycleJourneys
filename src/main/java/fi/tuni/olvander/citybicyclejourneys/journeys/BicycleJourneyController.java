b8a50d652624f321af9de3b2e522a2a92de77698
Olvander <olli.pertovaara@gmail.com>
2024-01-15 11:06:06 +0200

    Update file BicycleJourneyController.java
    Add method getAllJourneysCount()

--- src/main/java/fi/tuni/olvander/citybicyclejourneys/journeys/BicycleJourneyController.java
+++ src/main/java/fi/tuni/olvander/citybicyclejourneys/journeys/BicycleJourneyController.java
@@ -3,7 +3,12 @@ package fi.tuni.olvander.citybicyclejourneys.journeys;
 import fi.tuni.olvander.citybicyclejourneys.stations.Station;
 import fi.tuni.olvander.citybicyclejourneys.stations.StationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 
@@ -22,5 +27,23 @@ public class BicycleJourneyController {
     private ArrayList<Station> stations;
 
     public BicycleJourneyController() {}
+
+    @RequestMapping(value = "api/journeysCount/", method = RequestMethod.GET)
+    public synchronized ResponseEntity<Long> getAllJourneysCount() {
+        HttpHeaders headers = new HttpHeaders();
+        Long count;
+
+        headers.setAccessControlAllowOrigin("*");
+
+        if (this.allJourneys == null) {
+            count = this.bicycleJourneyDb.count();
+        } else {
+            count = ((ArrayList<BicycleJourney>) this.allJourneys)
+                    .stream()
+                    .count();
+        }
+
+        return new ResponseEntity<>(count, headers, HttpStatus.OK);
+    }
 
 }
