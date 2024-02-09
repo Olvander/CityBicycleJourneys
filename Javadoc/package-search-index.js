3e9f76fa8c6b68d802ab2c110a1d2f6a5459aac5
Olvander <olli.pertovaara@gmail.com>
2024-02-09 17:27:10 +0200

    Add Javadoc
    To describe the project backend classes, interfaces, methods and fields

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fi.tuni.olvander.citybicyclejourneys"},{"l":"fi.tuni.olvander.citybicyclejourneys.exceptions"},{"l":"fi.tuni.olvander.citybicyclejourneys.journeys"},{"l":"fi.tuni.olvander.citybicyclejourneys.stations"}];updateSearchResults();
