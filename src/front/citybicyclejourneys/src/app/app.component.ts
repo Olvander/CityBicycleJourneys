965d1b3aeb5c1e847441bf0ed9834a20410e8031
Olvander <olli.pertovaara@gmail.com>
2024-02-10 10:29:34 +0200

    Update 3 app.component files in frontend
    Add code to the root component app.component files to render the app

--- src/front/citybicyclejourneys/src/app/app.component.ts
+++ src/front/citybicyclejourneys/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, HttpClientModule, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'citybicyclejourneys';
+
+  reload(url: string): void {
+
+    if (window.location.href.endsWith(url)) {
+      window.location.reload();
+    }
+  }
 }
