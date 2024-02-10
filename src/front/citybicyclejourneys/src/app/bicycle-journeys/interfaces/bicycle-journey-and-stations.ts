e5149665289b7e43f9576688b1e659098c226924
Olvander <olli.pertovaara@gmail.com>
2024-02-10 11:59:49 +0200

    Update the frontend file BicycleJourneyAndStations
    To include the interface file contents

--- src/front/citybicyclejourneys/src/app/bicycle-journeys/interfaces/bicycle-journey-and-stations.ts
+++ src/front/citybicyclejourneys/src/app/bicycle-journeys/interfaces/bicycle-journey-and-stations.ts
@@ -1,2 +1,9 @@
+import { Station } from "../../bicycle-stations/interfaces/station";
+
 export interface BicycleJourneyAndStations {
+  id: number;
+  departureStation: Station | undefined;
+  returnStation: Station | undefined;
+  coveredDistance: number;
+  journeyDuration: number;
 }
