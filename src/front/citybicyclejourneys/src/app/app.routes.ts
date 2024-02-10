4720d9b82f298b8d34ebd2447efe0d522a149fff
Olvander <olli.pertovaara@gmail.com>
2024-02-10 10:52:53 +0200

    Update app.routes.ts in frontend
    Add the LicenseComponent to the routes file

--- src/front/citybicyclejourneys/src/app/app.routes.ts
+++ src/front/citybicyclejourneys/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from
     './page-not-found/page-not-found.component';
+import { LicenseComponent } from './license/license.component';
 
 export const routes: Routes = [
+  {path: 'license', component: LicenseComponent},
   {path: '**', redirectTo: 'not-found'},
   {path: 'not-found', component: PageNotFoundComponent}
 ];
