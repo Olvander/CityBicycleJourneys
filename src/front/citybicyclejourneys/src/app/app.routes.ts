c6421406e341800283c2bc634733f28efaad5b3d
Olvander <olli.pertovaara@gmail.com>
2024-02-10 11:21:48 +0200

    Update app.routes in frontend
    To include StationComponent in routes.ts

--- src/front/citybicyclejourneys/src/app/app.routes.ts
+++ src/front/citybicyclejourneys/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from
     './page-not-found/page-not-found.component';
 import { LicenseComponent } from './license/license.component';
+import { StationComponent } from './bicycle-stations/station/station.component';
 
 export const routes: Routes = [
   {path: 'license', component: LicenseComponent},
+  {path: "stations/:id", component: StationComponent, pathMatch: 'full'},
   {path: '**', redirectTo: 'not-found'},
   {path: 'not-found', component: PageNotFoundComponent}
 ];
