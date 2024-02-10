cd4b341136482e600fa4caad3e17f8338e127d8e
Olvander <olli.pertovaara@gmail.com>
2024-02-10 11:52:28 +0200

    Add JourneyListComponent to frontend
    Add 3 files for displaying Bicycle Journey data in frontend

---
+++ src/front/citybicyclejourneys/src/app/bicycle-journeys/journey-list/journey-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journey-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './journey-list.component.html',
+  styleUrl: './journey-list.component.css'
+})
+export class JourneyListComponent {
+
+}
