8ee936db74eed95be6d9219958535024a4a46b05
Olvander <olli.pertovaara@gmail.com>
2024-02-10 11:54:48 +0200

    Update app.routes in frontend
    Add JourneyListComponent to the routes

--- src/front/citybicyclejourneys/src/app/app.routes.ts
+++ src/front/citybicyclejourneys/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from
     './page-not-found/page-not-found.component';
 import { LicenseComponent } from './license/license.component';
+import { JourneyListComponent } from
+    './bicycle-journeys/journey-list/journey-list.component';
 import { StationComponent } from './bicycle-stations/station/station.component';
 import { StationListComponent } from
     './bicycle-stations/station-list/station-list.component';
@@ -9,6 +11,7 @@ import { StationListComponent } from
 export const routes: Routes = [
   {path: '', redirectTo: 'journeys', pathMatch: 'full'},
   {path: 'license', component: LicenseComponent},
+  {path: 'journeys', component: JourneyListComponent, pathMatch: 'full'},
   {path: "stations", component: StationListComponent, pathMatch: 'full'},
   {path: "stations/:id", component: StationComponent, pathMatch: 'full'},
   {path: '**', redirectTo: 'not-found'},
