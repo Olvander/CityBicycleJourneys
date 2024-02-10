e8ce66a55051821cec513b1e5b0bdc81fba5df21
Olvander <olli.pertovaara@gmail.com>
2024-02-10 10:53:59 +0200

    Add component month-selection-menu to frontend
    Add 3 component files

---
+++ src/front/citybicyclejourneys/src/app/month-selection-menu/month-selection-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-month-selection-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './month-selection-menu.component.html',
+  styleUrl: './month-selection-menu.component.css'
+})
+export class MonthSelectionMenuComponent {
+
+}
