f4387de0b89860cae7684985cbb1e9f7e52a0470
Olvander <olli.pertovaara@gmail.com>
2024-02-10 10:46:43 +0200

    Update file number-helper.service
    Add 3 functions

--- src/front/citybicyclejourneys/src/app/other services/number-helper.service.ts
+++ src/front/citybicyclejourneys/src/app/other services/number-helper.service.ts
@@ -5,5 +5,39 @@ import { Injectable } from '@angular/core';
 })
 export class NumberHelperService {
 
-  constructor() { }
+  constructor() {}
+
+  haveSameValues(a: number[], b: number[]): boolean {
+    
+    if (a.length === b.length) {
+      
+      for (let i = 0; i < a.length; i++) {
+        
+        if (a[i] !== b[i]) {
+          
+          return false;
+        }
+      }
+    
+    } else {
+      
+      return false;
+    }
+    
+    return true;
+  }
+
+  getMinutes(seconds: number): string {
+    let mins: number = seconds / 60;
+    let minutes: string = mins.toFixed(1);
+    
+    return minutes;
+  }
+
+  getKilometers(meters: number): string {
+    let kms: number = meters / 1000;
+    let kilometers: string = kms.toFixed(2);
+    
+    return kilometers;
+  }
 }
