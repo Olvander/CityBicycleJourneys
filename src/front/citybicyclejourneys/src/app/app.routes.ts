8586d4a2c3a266e2ecda43ab5f0678355ad4c29e
Olvander <olli.pertovaara@gmail.com>
2024-02-10 11:27:49 +0200

    Update app.routes.ts in frontend
    Add StationListComponent to the routes

--- src/front/citybicyclejourneys/src/app/app.routes.ts
+++ src/front/citybicyclejourneys/src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { PageNotFoundComponent } from
     './page-not-found/page-not-found.component';
 import { LicenseComponent } from './license/license.component';
 import { StationComponent } from './bicycle-stations/station/station.component';
+import { StationListComponent } from
+    './bicycle-stations/station-list/station-list.component';
 
 export const routes: Routes = [
+  {path: '', redirectTo: 'journeys', pathMatch: 'full'},
   {path: 'license', component: LicenseComponent},
+  {path: "stations", component: StationListComponent, pathMatch: 'full'},
   {path: "stations/:id", component: StationComponent, pathMatch: 'full'},
   {path: '**', redirectTo: 'not-found'},
   {path: 'not-found', component: PageNotFoundComponent}
