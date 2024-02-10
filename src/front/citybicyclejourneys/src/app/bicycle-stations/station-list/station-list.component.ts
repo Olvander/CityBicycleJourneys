490a05cdf1f73c82bbae4f34e3d9eebedcf5f059
Olvander <olli.pertovaara@gmail.com>
2024-02-10 11:24:49 +0200

    Add 3 StationListComponent files to frontend
    For displaying Bicycle Stations as a list

---
+++ src/front/citybicyclejourneys/src/app/bicycle-stations/station-list/station-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-station-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './station-list.component.html',
+  styleUrl: './station-list.component.css'
+})
+export class StationListComponent {
+
+}
