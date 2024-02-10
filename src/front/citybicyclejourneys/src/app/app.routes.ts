2b41dd1ce45db2bdaa1bbd8a499f7e0987b37442
Olvander <olli.pertovaara@gmail.com>
2024-02-10 10:41:19 +0200

    Update file app.routes in frontend
    Add PageNotFoundComponent to the app.routes.ts file

--- src/front/citybicyclejourneys/src/app/app.routes.ts
+++ src/front/citybicyclejourneys/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from
+    './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '**', redirectTo: 'not-found'},
+  {path: 'not-found', component: PageNotFoundComponent}
+];
